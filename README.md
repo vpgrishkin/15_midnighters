5d41c24c8f1067f0b0a510fd2972d26a66768063
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-04-09 16:17:46 +0300

    done

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+The program shows users who sent for review a task (on devman.org) from midnight to 5 a.m.
+
+# How to use
+
+1. virtualenv -p python3 env
+2. source env/bin/activate
+3. pip install -r requirements.txt
+4. python seek_dev_nighters.py
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,42 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+from pytz import timezone
+from datetime import datetime, time
 
-def get_midnighters():
-    pass
+import requests
+
+
+DEVMAN_API_URL = 'http://devman.org/api/challenges/solution_attempts'
+MIDNIGHT = time(0, 0, 0)
+NIGHT_END = time(5, 0, 0)
+
+
+def load_attempts():
+    params = {'page': 1}
+    devman_data = requests.get(DEVMAN_API_URL, params).json()
+    number_of_pages = int(devman_data['number_of_pages'])
+    for page in range(1, number_of_pages + 1):
+        params = {'page': page}
+        devman_data = requests.get(DEVMAN_API_URL, params).json()
+        print('Page {} was downloaded'.format(page))
+        yield from devman_data['records']
+
+
+def get_midnighters(records):
+    midnighters_set = set()
+    for record in records:
+        if (record['timestamp'] and record['timezone']) is None:
+            print('Warning: timestamp or timezone is None')
+            continue
+        local_date_time = datetime.fromtimestamp(float(record['timestamp'],),
+                                                 tz=timezone(record['timezone']))
+        user_time = local_date_time.time()
+        if MIDNIGH < user_time < NIGHT_END:
+            midnighters_set.add(record['username'])
+    return midnighters_set
+
 
 if __name__ == '__main__':
-  pass
+    records = load_attempts()
+    midnighters = get_midnighters(records)
+    print('\nMidnighters:')
+    for midnighter in midnighters:
+        print(midnighter)
