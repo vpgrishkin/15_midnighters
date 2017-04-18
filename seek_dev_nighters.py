df8d6b68f0a4ece94e8afb679fe233c7258ee1f8
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-04-18 14:31:28 +0300

    Fixed remarks

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,42 +1,55 @@
-from pytz import timezone
 from datetime import datetime, time
 
+from pytz import timezone
 import requests
 
 
 DEVMAN_API_URL = 'http://devman.org/api/challenges/solution_attempts'
-MIDNIGHT = time(0, 0, 0)
-NIGHT_END = time(5, 0, 0)
+MIDNIGHT = 0
+NIGHT_END = 5
 
 
 def load_attempts():
-    params = {'page': 1}
-    devman_data = requests.get(DEVMAN_API_URL, params).json()
-    number_of_pages = int(devman_data['number_of_pages'])
-    for page in range(1, number_of_pages + 1):
+    number_of_pages = None
+    if number_of_pages is None:
+        params = {'page': 1}
+        devman_data = requests.get(DEVMAN_API_URL, params).json()
+        number_of_pages = int(devman_data['number_of_pages'])
+        yield from devman_data['records']
+    
+    for page in range(2, number_of_pages + 1):
         params = {'page': page}
         devman_data = requests.get(DEVMAN_API_URL, params).json()
-        print('Page {} was downloaded'.format(page))
         yield from devman_data['records']
 
 
 def get_midnighters(records):
     midnighters_set = set()
+    warnings = []
     for record in records:
-        if (record['timestamp'] and record['timezone']) is None:
-            print('Warning: timestamp or timezone is None')
+        if record['timestamp'] is None:
+            warnings.append('User: {}. Warning: timestamp is None'.format(record['username']))
+            continue
+        if record['timezone'] is None:
+            warnings.append('User: {}. Warning: timezone is None'.format(record['username']))
             continue
         local_date_time = datetime.fromtimestamp(float(record['timestamp'],),
                                                  tz=timezone(record['timezone']))
         user_time = local_date_time.time()
-        if MIDNIGH < user_time < NIGHT_END:
+        if MIDNIGHT < user_time.hour < NIGHT_END:
             midnighters_set.add(record['username'])
-    return midnighters_set
+    if warnings == []:
+        warnings = ['Everything is ok: no empty timestamp and timezone']
+    return midnighters_set, warnings
 
 
 if __name__ == '__main__':
     records = load_attempts()
-    midnighters = get_midnighters(records)
+    midnighters, warnings = get_midnighters(records)
+    print('\nWarnings:')
+    for warning in warnings:
+        print(warning)
+    
     print('\nMidnighters:')
     for midnighter in midnighters:
         print(midnighter)
